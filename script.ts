7e959b68278cbe26e6c87c218429427d164e8895
sebastianwojciechowski6 <sebastianwojciechowski@microsoft.wsei.edu.pl>
2020-03-23 18:08:33 +0100

    minimax implementing

--- script.ts
+++ script.ts
@@ -1,31 +1,75 @@
 class Board {
     gameStarted: boolean = true;
     cellsVisible: HTMLTableCellElement[][];
-    // cellsForPoints: number[][] = [
-    //     [0,0,0],
-    //     [0,0,0],
-    //     [0,0,0]
-    // ];
+    humanPlayer: Player;
+    computerPlayer: Player;
+    cellsForPoints: number[][] = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+    ];
 
     constructor(cells: HTMLTableCellElement[][]) {
         this.cellsVisible = cells;
     }
 
+    playersRaffle() {
+        return Math.floor(Math.random() * 2) + 1;
+    }
+
+    playerChecker(sign: string) {
+        if (sign == 'X')
+            return 0;
+        else
+            return 1;
+    }
+
+    startGame() {
+        this.humanPlayer = new Player(this.playersRaffle());
+        this.computerPlayer = new Player(this.playerChecker(this.humanPlayer.sign));
+
+    }
+
     clickCell(x: number, y: number) {
         let arrayIndexer: number = 1;
+
         let cellPositionX: number = x - arrayIndexer;
         let cellPositionY: number = y - arrayIndexer;
 
         if (this.gameStarted) {
-            if (this.cellsVisible[cellPositionX][cellPositionY].innerHTML == 'X') {
-                console.log('There is an AI sign!')
-            } else {
-                if (this.cellsVisible[cellPositionX][cellPositionY].innerHTML == 'O') {
-                    console.log('There is your sign!')
-                } else {
-                    this.cellsVisible[cellPositionX][cellPositionY].innerHTML == 'O';
-                    // this.cellsForPoints[cellPositionX][cellPositionY] = 1;
+            // Click on computer player's sign option.
+            if (this.cellsVisible[cellPositionX][cellPositionY].innerHTML == this.computerPlayer.sign) {
+                console.log("There is an computer's sign!");
+            }
+            else {
+                // Click on human player's sign option.
+                if (this.cellsVisible[cellPositionX][cellPositionY].innerHTML == this.humanPlayer.sign) {
+                    console.log('There is your sign!');
+                }
+                // Click on empty cell.
+                else {
+                    // Write sign of human player to cell.
+                    this.cellsVisible[cellPositionX][cellPositionY].innerHTML == this.humanPlayer.sign;
+                    // Add 1 point to cells array.
+                    this.cellsForPoints[cellPositionX][cellPositionY] = 1;
+                    // Check if that move causes the win.
+                    if (this.checkWinner() == this.humanPlayer) {
+                        this.gameStarted = false;
+                        alert("Human Player has won!");
+                    }
+                    // Check if that move causes the draw.
+                    else {
+                        if (this.checkDraw()) {
+                            this.gameStarted = false;
+                            alert("It's a draw!");
+                        }
+                        // Game is still on, human putted a sign, time for computer player.
+                        else {
 
+
+
+                        }
+                    }
 
                 }
             }
@@ -37,22 +81,38 @@ class Board {
         return (a == b && b == c && a.innerHTML != '')
     }
 
-    checkWin() {
-        let winner: string;
+    checkDraw() {
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (this.cellsForPoints[i][j] == 0)
+                    return false;
+            }
+        }
+        return true;
+
+    }
+
+    checkWinner() {
+        let winningSign: string;
 
         for (let i = 0; i < 3; i++) {    //Horizontal
             if (this.threeEquality(this.cellsVisible[i][0], this.cellsVisible[i][1], this.cellsVisible[i][2]))
-                winner = this.cellsVisible[i][0].innerHTML;
+                winningSign = this.cellsVisible[i][0].innerHTML;
         }
         for (let i = 0; i < 3; i++) {    //Vertical
             if (this.threeEquality(this.cellsVisible[0][i], this.cellsVisible[0][i], this.cellsVisible[0][i]))
-                winner = this.cellsVisible[0][i].innerHTML;
+                winningSign = this.cellsVisible[0][i].innerHTML;
         }
         //Diagonal
         if (this.threeEquality(this.cellsVisible[0][0], this.cellsVisible[1][1], this.cellsVisible[2][2]))
-            winner = this.cellsVisible[0][0].innerHTML;
+            winningSign = this.cellsVisible[0][0].innerHTML;
         if (this.threeEquality(this.cellsVisible[0][2], this.cellsVisible[1][1], this.cellsVisible[2][0]))
-            winner = this.cellsVisible[0][2].innerHTML;
+            winningSign = this.cellsVisible[0][2].innerHTML;
+
+        if (winningSign == this.humanPlayer.sign)
+            return this.humanPlayer;
+        else
+            return this.computerPlayer;
     }
 }
 
@@ -67,26 +127,13 @@ class Player {
         else
             console.log('Player number error.')
     }
-
-    GetSign() {
-        return this.sign;
-    }
-}
-
-class Game {
-    Round() {
-        // if()
-        // return 2;
-        // if()
-        // return 1;
-    }
 }
 
 document.addEventListener("DOMContentLoaded", appStart);
 
 function appStart() {
     let cell11: HTMLTableCellElement
-        = <HTMLTableCellElement>document.querySelector('#cel11');
+        = <HTMLTableCellElement>document.querySelector('#cell11');
     let cell12: HTMLTableCellElement
         = <HTMLTableCellElement>document.querySelector('#cell12');
     let cell13: HTMLTableCellElement
